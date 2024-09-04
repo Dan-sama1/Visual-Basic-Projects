8d5af53f74d7d9881862ddd4acb6d016d8114b77
Dan-sama1 <danielbarrameda2021@gmail.com>
2024-09-04 18:31:22 +0800

    basic clock done
    lamaw

---
+++ Digital Clock/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmClock
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblClock2 = New System.Windows.Forms.Label()
+        Me.tmrTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblClock2
+        '
+        Me.lblClock2.AutoSize = True
+        Me.lblClock2.Font = New System.Drawing.Font("Digital-7 Mono", 71.99999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblClock2.ForeColor = System.Drawing.Color.Red
+        Me.lblClock2.Location = New System.Drawing.Point(70, 68)
+        Me.lblClock2.Name = "lblClock2"
+        Me.lblClock2.Size = New System.Drawing.Size(535, 96)
+        Me.lblClock2.TabIndex = 1
+        Me.lblClock2.Text = "12:00:00 AM"
+        '
+        'tmrTimer
+        '
+        Me.tmrTimer.Enabled = True
+        Me.tmrTimer.Interval = 1000
+        '
+        'frmClock
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowText
+        Me.ClientSize = New System.Drawing.Size(711, 228)
+        Me.Controls.Add(Me.lblClock2)
+        Me.Name = "frmClock"
+        Me.Text = "Digital Clock"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblClock2 As Label
+    Friend WithEvents tmrTimer As Timer
+End Class
---
+++ Digital Clock/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmClock
+    Private Sub tmrTimer_Tick(sender As Object, e As EventArgs) Handles tmrTimer.Tick
+        'lblClock1.Text = TimeString ' 24 hour time
+        lblClock2.Text = DateTime.Now.ToString("hh:mm:ss tt")
+    End Sub
+
+End Class
